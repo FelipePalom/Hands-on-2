3c55e29a0a2bd12d58ab0338a3ed994721b7699d
FelipePalom <161984574+FelipePalom@users.noreply.github.com>
2024-03-03 00:16:24 -0600

    Update HelloWord.bat

--- HelloWord/HelloWord.bat
+++ HelloWord/HelloWord.bat
@@ -1,1 +1,17 @@
+@echo off
+
+:: Imprimir "Hello World" en pantalla
+echo Hello World
+
+:: Listar el contenido del directorio actual
+dir
+
+:: Crear un subdirectorio llamado Test
+mkdir Test
+
+:: Cambiarse al subdirectorio Test
+cd Test
+
+:: Listar el contenido del subdirectorio Test
+dir
 
